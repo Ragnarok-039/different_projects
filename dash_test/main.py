29d0fa72e71cb210948f7b89f534a1c7b169e450
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2023-04-26 15:37:59 +0200

    Update main.py

--- dash_test/main.py
+++ dash_test/main.py
@@ -1,15 +1,24 @@
 import pandas as pd
+import plotly.express as px
 from dash import Dash, dcc, html
+
+df = pd.read_csv(r'F:\Datasets\games-release-ALL.csv')
+df['release'] = pd.to_datetime(df['release'])
+df['year_release'] = df['release'].dt.year
+df['rating'] = df['rating'].astype(float)
+
+print(df.head())
+print(df.columns)
+
+new_df = df[['year_release', 'rating']].groupby('year_release').mean()
+print(new_df.head())
 
 app = Dash(__name__)
 
-df = pd.read_csv(r'F:\Datasets\games-release-ALL.csv')
-
-print(df.head())
-
-app.layout = html.Div([
-    dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'], 'Montréal')
-])
+app.layout = html.Div(children=[
+    html.Div(dcc.Dropdown(df['year_release'].unique())),
+    html.Div(dcc.Graph(id='new', figure=px.line(df, x='year_release',
+                                                y='rating')))])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
