1f854e1d21486a346ed12d8d3e12a70e48bd0693
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2023-04-27 09:05:49 +0200

    Первая рабочая версия дашборда

--- dash_test/main.py
+++ dash_test/main.py
@@ -5,19 +5,20 @@ from dash import Dash, dcc, html
 df = pd.read_csv(r'F:\Datasets\games-release-ALL.csv')
 df['release'] = pd.to_datetime(df['release'])
 df['year_release'] = df['release'].dt.year
-df['rating'] = df['rating'].astype(float)
+df['rating'] = df['rating'].apply(lambda x: x.strip('%')).astype(float)
 
 print(df.head())
 print(df.columns)
 
-new_df = df[['year_release', 'rating']].groupby('year_release').mean()
+new_df = df[['year_release', 'rating']].groupby('year_release').agg('mean')
 print(new_df.head())
+print(new_df.columns)
 
 app = Dash(__name__)
 
 app.layout = html.Div(children=[
     html.Div(dcc.Dropdown(df['year_release'].unique())),
-    html.Div(dcc.Graph(id='new', figure=px.line(df, x='year_release',
+    html.Div(dcc.Graph(id='new', figure=px.line(new_df, x=new_df.index,
                                                 y='rating')))])
 
 if __name__ == '__main__':
